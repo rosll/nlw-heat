bc55a56138e3603bb7c7c3992a3a8decb455dc97
Romulo Lima <romulosilva-07@hotmail.com>
2021-11-21 17:19:22 -0300

    projeto mobile finalizado

--- app/src/components/Header/index.tsx
+++ app/src/components/Header/index.tsx
@@ -3,18 +3,22 @@ import { View, Text, TouchableOpacity } from 'react-native';
 import { UserPhoto } from '../UserPhoto'
 import { styles } from './styles';
 import LogoSvg from '../../assets/logo.svg'
+import { useAuth } from '../../hooks/auth';
 
 export function Header(){
+  const { user, signOut } = useAuth()
   return (
     <View style={styles.container}>
       <LogoSvg />
       <View style={styles.logoutButton}>
+        
+        {user &&
+          <TouchableOpacity onPress={signOut}>
+            <Text style={styles.logoutText}>Sair</Text>
+          </TouchableOpacity>
+        }
 
-        <TouchableOpacity>
-          <Text style={styles.logoutText}>Sair</Text>
-        </TouchableOpacity>
-
-        <UserPhoto imageUri='https://github.com/rosll.png' />
+        <UserPhoto imageUri={user?.avatar_url} />
     
       </View>
     </View>
